078b18da0e7640c178f5bae81cbf5452254ee9df
fanfan <fanfan12@aliyun.com>
2018-01-30 15:49:53 +0800

    start

--- statistics/views.py
+++ statistics/views.py
@@ -1,9 +1,18 @@
+# coding=utf-8
 from django.shortcuts import render
+from statistics import models
+
+import time
 
 # Create your views here.
 
 
 def index(request):
+    app_id = 12345
+    # 今日时间的00:00:00转换为时间戳
+    yes_time = int(time.time()) - int(time.time()%86400) + time.timezone
+
+    res = models.UserInfoForStatistics.objects.filter(app_id=app_id)
     return render(request, 'static.html', {'res': {}})
 
 
