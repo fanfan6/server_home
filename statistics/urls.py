f53bfdc12dec757ab2540b3432774055a1fb65b1
fanfan <fanfan12@aliyun.com>
2018-01-30 11:05:29 +0800

    check

--- statistics/urls.py
+++ statistics/urls.py
@@ -5,4 +5,6 @@ from statistics import views
 
 urlpatterns = [
     url(r'^index$', views.index),
+    url(r'^app_pass$', views.app_pass),
+    url(r'^mod_grade', views.mod_grade),
 ]
--- statistics/views.py
+++ statistics/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'static.html', {'res': {}})
+
+
+def app_pass(request):
+    return render(request, 'app_pass.html', {'res':{}})
+
+
+def mod_grade(request):
+    return render(request, 'mod_grade.html', {'res':{}})
